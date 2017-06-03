60338f4120f8a3f407cd9118eb9954ba5b986175
thebsdbox <daniel.finneran@gmail.com>
2017-06-03 11:00:27 +0100

    Fixed up a simple CLI/Flagset to build a dynamic OVF

--- main.go
+++ main.go
@@ -2,12 +2,46 @@ package main
 
 import (
 	"encoding/xml"
+	"flag"
 	"fmt"
 	"os"
 	"path"
+	"path/filepath"
+	"strings"
+
+	log "github.com/Sirupsen/logrus"
 )
 
 func main() {
+
+	invoked := filepath.Base(os.Args[0])
+
+	network := flag.String("network", "", "The network label the VM will use")
+	cpus := flag.String("cpus", "1", "Number of CPUs")
+	mem := flag.String("mem", "1024", "Amount of memory in MB")
+
+	flag.Usage = func() {
+		fmt.Printf("USAGE: %s push vcenter [options] path \n\n", invoked)
+		fmt.Printf("'path' specifies the full path of an image that will be pushed\n")
+		fmt.Printf("Options:\n\n")
+		flag.PrintDefaults()
+	}
+
+	flag.Parse()
+
+	remArgs := flag.Args()
+	if len(remArgs) == 0 {
+
+		fmt.Printf("Please specify the path to the image to push\n")
+		flag.Usage()
+		os.Exit(1)
+	}
+	isoPath := remArgs[0]
+
+	// Ensure an iso has been passed to the vCenter push Command
+	if !strings.HasSuffix(isoPath, ".iso") {
+		log.Fatalln("Please pass an \".iso\" file as the path")
+	}
 
 	var envelope Envelope
 
@@ -15,7 +49,7 @@ func main() {
 
 	var net NetSection
 	net.NetInfo = "List of Networks"
-	net.Network.NetName = "VM Network"
+	net.Network.NetName = *network
 	net.Network.NetDesc = "Default Network"
 	envelope.Net = &net
 
@@ -24,21 +58,20 @@ func main() {
 	newHardware.VHWSystem.VHWSystemType = "vmx-11"
 	newHardware.VHWSystem.VHWSystemID = "Other Linux 3.x kernel 64-bit"
 	newHardware.VHWSystem.VHWSystemName = "Virtual Hardware Family"
-	addMemoryToVM(&newHardware, "2048")
-	addCPUtoVM(&newHardware, "4")
+	addMemoryToVM(&newHardware, *mem)
+	addCPUtoVM(&newHardware, *cpus)
 
 	ideController := addIDEControllerToVM(&newHardware)
 	//	scsiController := addSCSIControllertoVM(&newHardware)
 
-	cdFilePath := "./vcenter.iso"
-
-	fi, e := os.Stat(cdFilePath)
+	fi, e := os.Stat(isoPath)
 	if e != nil {
 		os.Exit(1)
 	}
+
 	var newDiskSection DiskSection
-	addCDToController(&newHardware, ideController, cdFilePath)
-	appendFilesToReferences(&envelope, path.Base(cdFilePath), "file1", fmt.Sprintf("%d", fi.Size()))
+	addCDToController(&newHardware, ideController, isoPath)
+	appendFilesToReferences(&envelope, path.Base(isoPath), "file1", fmt.Sprintf("%d", fi.Size()))
 
 	// ISOs dont go in the disk section OBVIOUSLY :'''(
 	//appdendDiskToDiskSection(&newDiskSection, "8", "byte * 2^30", "file1", "vmdisk1", "http://format", "0")
